e4daad70d43b335097899203f874d80616d631bc
“kukim” <kukim.dev@gmail.com>
2022-11-20 16:58:39 +0700

    feat: Testcontainers를 활용하여 redis 사용하기
    - 간단하게 테스트에 레디스 컨테이너 생성
    - logback 설정

---
+++ src/test/java/kim/ku/redis/testcontainers/RedisTestContaners.java
@@ -0,0 +1,22 @@
+package kim.ku.redis.testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+class RedisTestContaners {
+
+	@Container
+	public GenericContainer redis = new GenericContainer("redis:7.0.5")
+			.withExposedPorts(6379);
+
+	@Test
+	void testContainers_실행확인() {
+		assertThat(redis.getHost()).isEqualTo("localhost");
+		assertThat(redis.getExposedPorts()).contains(6379);
+	}
+}
